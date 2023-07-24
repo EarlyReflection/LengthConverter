9f541439a3ee42b68de8f72428f24de7e96295a0
Vladimir Dvornikov <earlyreflections@protonmail.com>
2023-07-25 00:09:50 +0300

    Initial Commit

---
+++ LengthConverter/LengthConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LengthConverterApp.swift
+//  LengthConverter
+//
+//  Created by Vladimir Dvornikov on 25/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct LengthConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
