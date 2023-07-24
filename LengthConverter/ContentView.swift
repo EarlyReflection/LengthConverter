d5c819dd99baa32a696999c56a6bccf61d5051d2
Vladimir Dvornikov <earlyreflections@protonmail.com>
2023-07-25 00:43:19 +0300

    Added first segmented control

--- LengthConverter/ContentView.swift
+++ LengthConverter/ContentView.swift
@@ -8,14 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var value = 0
+    
+let lengths = ["meter", "kilometer", "feet", "yard", "mile"]
+    
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Form {
+            Section {
+                Picker("Legths", selection: $value) {
+                    ForEach(lengths, id: \.self) {
+                        Text($0)
+                    }
+                }
+                .pickerStyle(.segmented)
+            
+            }
         }
-        .padding()
     }
 }
 
